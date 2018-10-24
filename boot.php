39d7bb665a29aff90cfba82994787be9b665ec34
alex.kovalevv <alex.kovalevv@gmail.com>
2018-10-24 15:12:05 +0300

    Обновлена версия модуля

--- boot.php
+++ boot.php
@@ -23,7 +23,7 @@
 
 	define('ONEPRESS_COMPILER_000_LOADED', true);
 
-	define('ONEPRESS_COMPILER_000_VERSION', '4.0.3');
+	define('ONEPRESS_COMPILER_000_VERSION', '4.0.4');
 
 	// Absolute path and URL to the files and resources of the module.
 	define('ONEPRESS_COMPILER_000_DIR', dirname(__FILE__));
