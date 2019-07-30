e1f071de68138d57042e1dabb6295119c91ea4c0
alex.kovalevv <alex.kovalevv@gmail.com>
2019-07-30 05:49:18 +0300

    Обновлена версия модуля. Добавлена константа с версией модуля.

--- boot.php
+++ boot.php
@@ -1,31 +1,31 @@
 <?php
 
-	/**
-	 * OnePress Compiler
-	 *
-	 * @author Alex Kovalev <alex.kovalevv@gmail.com>
-	 * @copyright (c) 2018, Webcraftic Ltd
-	 *
-	 * @package core
-	 * @since 1.0.0
-	 */
+/**
+ * OnePress Compiler
+ *
+ * @author        Alex Kovalev <alex.kovalevv@gmail.com>
+ * @since         1.0.0
+ * @package       core
+ * @copyright (c) 2018, Webcraftic Ltd
+ *
+ */
 
-	// Exit if accessed directly
-	if( !defined('ABSPATH') ) {
-		exit;
-	}
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	// Checks if the one is already loaded.
-	// We prevent to load the same version of the module twice.
-	if( defined('ONEPRESS_COMPILER_000_LOADED') ) {
-		return;
-	}
+// Checks if the one is already loaded.
+// We prevent to load the same version of the module twice.
+if ( defined( 'ONEPRESS_COMPILER_000_LOADED' ) ) {
+	return;
+}
 
-	define('ONEPRESS_COMPILER_000_LOADED', true);
+define( 'ONEPRESS_COMPILER_000_LOADED', true );
 
-	define('ONEPRESS_COMPILER_000_VERSION', '4.0.4');
+define( 'ONEPRESS_COMPILER_000_VERSION', '4.0.6' );
 
-	// Absolute path and URL to the files and resources of the module.
-	define('ONEPRESS_COMPILER_000_DIR', dirname(__FILE__));
+// Absolute path and URL to the files and resources of the module.
+define( 'ONEPRESS_COMPILER_000_DIR', dirname( __FILE__ ) );
 
-	include(ONEPRESS_COMPILER_000_DIR . '/compiler-functions.php');
+include( ONEPRESS_COMPILER_000_DIR . '/compiler-functions.php' );
